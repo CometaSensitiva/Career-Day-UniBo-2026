238d62688af1007d2820117ffb3b0bf410e95fed
CometaSensitiva <michelemazzatre@gmail.com>
2026-03-10 15:05:10 +0100

    Initial commit (history reset)

---
+++ firebase-public-config.js
@@ -0,0 +1,13 @@
+// Config Firebase pubblica del progetto web.
+// Nota: per le web app Firebase questi valori NON sono segreti.
+// Proteggi la chiave apiKey con restrizioni in Google Cloud Console.
+window.__FIREBASE_PUBLIC_CONFIG__ = {
+    apiKey: 'AIzaSyA8s3koXruK_Ghuw2Se_D13Br985kojpjY',
+    authDomain: 'career-day-8723a.firebaseapp.com',
+    databaseURL: 'https://career-day-8723a-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'career-day-8723a',
+    storageBucket: 'career-day-8723a.firebasestorage.app',
+    messagingSenderId: '356148580370',
+    appId: '1:356148580370:web:384906aa5342f1198cc8cb',
+    measurementId: 'G-02LLYVFR76'
+};
